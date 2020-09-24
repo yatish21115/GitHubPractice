17d7823df32b5e475b4408ad3712b72e18257921
yatish21115 <yashuyatish@gmail.com>
2020-09-25 03:12:23 +0530

    Updating for Practice

---
+++ CLASS1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CLASS1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int a = scan.nextInt();
+		System.out.println("The number is ");
+		System.out.println(a);
+		
+		Scanner scan1 = new Scanner(System.in);
+		System.out.println("Enter the decimal:");
+		float b = scan1.nextFloat();
+		System.out.println("The decimal is ");
+		System.out.println(b);
+		
+        Scanner scan2 = new Scanner(System.in);
+		System.out.println("Enter the line:");
+		String c = scan2.nextLine();
+		System.out.println("The line is ");
+		System.out.println(c);
+	}
+
+}
